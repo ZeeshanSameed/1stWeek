0ca189bb2647c7e204f0eecf39089ca5b8f8f077
ZeeshanSameed <zeeshanmalik6061@gmail.com>
2019-11-27 11:56:39 +0500

    errors are handled

--- python/WeatherReport.py
+++ python/WeatherReport.py
@@ -1,11 +1,37 @@
-import requests, json
-c = "yes"
-while c != "no":
-    api_address = 'http://api.openweathermap.org/data/2.5/weather?appid=d01ff7450d72d4b98014a97d3efe7dd0&q='
-    city = input ("Enter City Name :")
-    url = api_address + city
-    json_data = requests.get(url).json()
-    j_data = json_data['main']["temp"]
-    print(j_data)
-    print("Do you want to search for another cty name?")
-    c = input ("type yes or no \n")
+import requests
+
+
+APP_ID = 'd01ff7450d72d4b98014a97d3efe7dd0'
+API_ADDRESS = 'http://api.openweathermap.org/data/2.5/weather?appid={}&q='.format(APP_ID)
+
+choice = "yes"
+while choice == "yes" or choice == "Yes":
+    city = input("Enter city name: ")
+    print('\n', '*' * 50)
+
+    
+    #if isinstance(city,str):
+        
+    try:
+
+        json_data = requests.get("{}{}".format(API_ADDRESS, city)).json()
+        temprature_in_k = json_data['main']['temp']
+
+
+    # TODO: Check the response code. and make the proper flow
+    # TODO: make the function for conversions
+
+        celsius = temprature_in_k - 273.15
+        fahrenheit = temprature_in_k * 9/5 - 459.67
+
+        print("Temperature in {} will be:".format(city))
+        print("\tKelvin: ", temprature_in_k)
+        print("\tCelsius: ", celsius)
+        print("\tFahrenheit: ", fahrenheit)
+        print('*' * 50)
+        print("Do you want to search for another city?")
+        choice = input("Type yes or no: ")
+
+    except (IndexError,KeyError,ValueError,TypeError): 
+        print("Not a valid name do you want to search for another city?")
+        choice = input("Type yes or no: ")
--- python/WordSearchAp.py
+++ python/WordSearchAp.py
@@ -1,8 +1,8 @@
 import requests
 
 url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
-n="yes"
-while n != "no":
+choice="yes"
+while choice == "yes" or choice == "Yes":
     querystring = {"term":"wat"}
 
     headers = {
@@ -11,14 +11,22 @@ while n != "no":
         }
 
     querystring["term"] = input("Enter a word: \n")
-    n = querystring["term"]
+
+    #if isinstance(querystring["term"], str):
+    try:    
+        choice = querystring["term"]
 
 
-    response = requests.request("GET", url, headers=headers, params=querystring).json()
-    definitions = response['list']
-    definition = definitions[0]['definition']
-    example = definitions[0]['example']
-    print(definition)
-    print(example)
-    print("\n Do you want to search for another cty name?")
-    n = input ("type yes or no \n")
+        response = requests.request("GET", url, headers=headers, params=querystring).json()
+        definitions = response['list']
+        definition = definitions[0]['definition']
+        example = definitions[0]['example']
+    
+        print("Definiton is: \n",definition)
+        print("Examples would be:\n",example)
+        print("\n Do you want to search for another word?")
+        choice = input ("type yes or no \n")
+    
+    except(IndexError,KeyError,ValueError,TypeError):
+        print("\n Not a valid word do you want to search for another word?")
+        choice = input ("type yes or no \n")
